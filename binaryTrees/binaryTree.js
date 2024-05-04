36562e64a3b6a69c037d1b939ace9479ed9c67df
Stilram19 <bednaouiomar19@gmail.com>
2024-05-04 19:28:57 +0100

    depth first search & breadth first search in binaryTree

--- binaryTrees/binaryTree.js
+++ binaryTrees/binaryTree.js
@@ -6,19 +6,20 @@ class Node {
     }
 }
 
-// depth first search
-const dfs = (root) => {
+// the depth first traversal
+
+const IterDFS = (root) => {
     if (root == null) {
-        return ;
+        return [];
     }
 
-    let stack = [root];
+    let stack = [ root ];
+    let values = [];
 
     while (stack.length > 0) {
-        let current = stack.pop();
+       let current = stack.pop();
 
-        console.log(current.val);
-
+        values.push(current.val);
         if (current.right != null) {
             stack.push(current.right);
         }
@@ -27,15 +28,62 @@ const dfs = (root) => {
             stack.push(current.left);
         }
     }
+
+    return (values);
 }
 
-const recDfs = (current) => {
-    if (current == null) {
-        return ;
+//     a
+//   b        c
+// d   e        f
+
+// call stack:
+
+// STD_OUT: dbeacf
+
+const recFDT = (root) => {
+    if (root == null) {
+        return [];
     }
-    console.log(current.val);
-    recDfs(current.left);
-    recDfs(current.right);
+
+    let left = recFDT(root.left);
+    let right = recFDT(root.right);
+
+    return [root.val, ...left, ...right];
+}
+
+// Breadth first traversal:
+
+//        a
+//   b         c
+// d   e     f   g
+
+// queue:
+// current: 
+// out: abcdefg
+
+const BFT = (root) => {
+    if (root === null) {
+        return [];
+    }
+
+    const queue = [root];
+    const result = [];
+
+    while (queue.length > 0) {
+        let current = queue.shift();
+
+        result.push(current.val);
+
+        if (current.left != null) {
+            queue.push(current.left);
+        }
+
+        if (current.right != null) {
+            queue.push(current.right);
+        }
+    }
+
+    return (result);
 }
 
 const a = new Node('a');
@@ -44,11 +92,23 @@ const c = new Node('c');
 const d = new Node('d');
 const e = new Node('e');
 const f = new Node('f');
+const g = new Node('g');
 
 a.left = b;
 a.right = c;
 b.left = d;
 b.right = e;
 c.left = f;
+c.right = g;
 
-recDfs(a);
+// const vals = IterDFS(a);
+
+// console.log(vals);
+
+// const vals = recBFT(a);
+
+// console.log(vals);
+
+const vals = BFT(a);
+
+console.log(vals);
