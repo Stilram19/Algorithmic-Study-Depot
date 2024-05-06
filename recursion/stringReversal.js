e0571a2916dd78f17f16dcd27aaf41832cb916e1
Stilram19 <bednaouiomar19@gmail.com>
2024-05-06 16:53:26 +0100

    reverseString recusively

---
+++ recursion/stringReversal.js
@@ -0,0 +1,11 @@
+const reverseString = (str) => {
+    if (str.length <= 1) {
+        return str; // Base case: return the string itself when its length is 0 or 1
+    }
+
+    const remainingString = str.slice(1);
+    return reverseString(remainingString) + str[0]; // Recursive call with remaining string
+}
+
+const str = "The Average Programmer";
+console.log(reverseString(str));
