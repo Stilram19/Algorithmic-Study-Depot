5b64a19d4284d3f3adb5cb27ec26f5eeae84338a
Stilram19 <bednaouiomar19@gmail.com>
2024-05-13 01:40:30 +0100

    merge two sorted linked lists

--- recursion/mergeSortedLinkedLists.js
+++ recursion/mergeSortedLinkedLists.js
@@ -5,6 +5,81 @@ class Node {
     }
 };
 
-const mergeSortedLinkedLists = (list1, list2) => {
-    
+const insertNode = (dest, src) => {
+    if (dest === null) {
+        return (new Node(src.val));
+    }
+
+    dest.next = new Node(src.val);
+    return (dest.next);
 }
+
+const mergeSortedLinkedLists = (list1, list2) => {
+    let mergeList = null;
+    let mergeListCurrent = null;
+
+    while (list1 != null && list2 != null) {
+        let correspondantNode = null;
+
+        if (list1.val < list2.val) {
+            correspondantNode = list1;
+            list1 = list1.next;
+        } else {
+            correspondantNode = list2;
+            list2 = list2.next;
+        }
+
+        mergeListCurrent = insertNode(mergeListCurrent, correspondantNode);
+
+        if (mergeList === null) {
+            mergeList = mergeListCurrent;
+        }
+    }
+
+    while (list1 != null) {
+        insertNode(mergeListCurrent, list1);
+        list1 = list1.next;
+    }
+
+    while (list2 != null) {
+        insertNode(mergeListCurrent, list2);
+        list2 = list2.next;
+    }
+
+    return (mergeList);
+}
+
+const displayList = (head) => {
+    let current = head;
+
+    while (current != null) {
+        console.log(current.val);
+        current = current.next;
+    }
+}
+
+let a = new Node(2);
+let b = new Node(4);
+let c = new Node(6);
+let d = new Node(8);
+let e = new Node(10);
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+let aa = new Node(1);
+let bb = new Node(5);
+let cc = new Node(7);
+let dd = new Node(9);
+let ee = new Node(11);
+
+aa.next = bb;
+bb.next = cc;
+cc.next = dd;
+dd.next = ee;
+
+// displayList(aa);
+
+displayList(mergeSortedLinkedLists(a, aa));
