9b34ff6c9ef33cbbad503310e0310da97463858b
Stilram19 <bednaouiomar19@gmail.com>
2024-05-31 09:30:25 +0100

    remove Deplicates with 2 pointers approach

--- 150_problems/removeDuplicates.ts
+++ 150_problems/removeDuplicates.ts
@@ -1,49 +1,16 @@
-// inline solution
-
-// 1, 2, 3, 4
-// 2, 3, 4
-
-function removeItem(nums: number[], index: number): void {
-    for (let i = index; i < nums.length - 1; i++) {
-        nums[i] = nums[i + 1];
-    }
-
-    nums.pop();
-}
-
-function deleteAdjacentDuplicates(nums: number[], compareValue: number, index: number): number {
-    for (let i = index; i < nums.length; i++) {
-        if (nums[i] != compareValue) {
-            return (i);
-        }
-        removeItem(nums, i);
-        i--;
-    }
-
-    return (-1);
-}
-
 function removeDuplicates(nums: number[]): number {
-    
-    for (let i = 0; i < nums.length; i++) {
-        let currentValue = nums[i];
-
-        // delete all duplicates of ${currentValue}
-        let nextIndex = deleteAdjacentDuplicates(nums, currentValue, i + 1);
-
-        if (nextIndex == -1) {
-            break ;
-        }
-
-        i = nextIndex - 1;
+    if (nums.length == 0) {
+        return (0);
     }
 
-    return (nums.length);
-};
+    let i = 0;
 
-const array = [1, 1, 1, 2, 2, 3];
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] != nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
 
-const k = removeDuplicates(array);
-
-console.log('k: ' + k);
-console.log('nums: ' + array);
+    return (i + 1);
+}
